fc38f76c2d255a05c97d3582b6bc41cd13828d87
daniel yesid cely robayo <danielcely1@gmail.com>
2023-06-12 23:30:29 -0500

    landing page iniciada

--- src/Places/Landing.jsx
+++ src/Places/Landing.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import miFoto from '../img/FotoII.png';
 
 
 export default function LandingPage(){
     
     return (<div>
-            <p>Hola Mundo</p>
+        <div className="contenedor landing">
+            <div className='imgPersona'>
+                <img src={miFoto} alt='mi foto'/>
+            </div>
+            <div className="InfoLanding">
+                <h1>Daniel Yesid Cely Robayo</h1>
+                <p>FrontEnd Developer</p>
+            </div>
+        </div>
     </div>)
 
 }

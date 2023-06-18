cf98d9d09fec7005cf348ee9e2225c44ca57520b
daniel yesid cely robayo <danielcely1@gmail.com>
2023-06-17 20:17:47 -0500

    avance en landing

--- src/Places/Landing.jsx
+++ src/Places/Landing.jsx
@@ -5,14 +5,14 @@ import miFoto from '../img/FotoII.png';
 
 export default function LandingPage(){
     
-    return (<div>
-        <div className="contenedor landing">
+    return (<div className="contenedor">
+        <div className="landing">
             <div className='imgPersona'>
                 <img src={miFoto} alt='mi foto'/>
             </div>
             <div className="InfoLanding">
                 <h1>Daniel Yesid Cely Robayo</h1>
-                <p>FrontEnd Developer</p>
+                <p>Frontend Developer</p>
                 <Link to='/' className='btn btn__Landing'>Conoceme</Link>
             </div>
         </div>

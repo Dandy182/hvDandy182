85d3b5800a37763e04d408a28e1f78e1336b5d1c
daniel yesid cely robayo <danielcely1@gmail.com>
2023-06-21 01:06:17 -0500

    avance en about

--- src/Places/About.jsx
+++ src/Places/About.jsx
@@ -8,8 +8,17 @@ export default function About(){
     <div className="about bg">
         <UpperBar />
         <main className="contenedor">
-            <p></p>
+            <article className="info__about">
+                <h2>Sobre mi</h2>
+
+            </article>
+            <article className="info__about">
+                <h2>Tecnologias con las he trabajado</h2>
+            </article>
         </main>
+        <div className="bg__portafolio">
+
+        </div>
         
         </div>)
 }

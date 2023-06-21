a7809558ea7727e1eabc8ecac25ae65e57c3a14f
daniel yesid cely robayo <danielcely1@gmail.com>
2023-06-21 00:23:47 -0500

    upperbar terminado

--- src/Components/UpperBar.jsx
+++ src/Components/UpperBar.jsx
@@ -11,18 +11,18 @@ export default function UpperBar(){
     const {nombre, foto, github, linkedIn} = yo;
 
     return(<nav className="upperBar">
-        <div className="contenedor">
+        <div className="contenedor bar">
             <div className="uppLeft">
                 <Foto img={foto} nameFoto={nombre} sFoto={'upperBar__foto'} />
                 <p>{nombre}</p>
                 </div>
             <div className="uppRight">
-            <Link target="_blank" to={github}>
-                <FontAwesomeIcon icon={faGithub} />
-            </Link>
-            <Link to={linkedIn} target="_blank">
-                <FontAwesomeIcon icon={faLinkedinIn} />
-            </Link>
+                <Link target="_blank" className="icon" to={github}>
+                    <FontAwesomeIcon className="svg" icon={faGithub} />
+                </Link>
+                <Link to={linkedIn} className="icon" target="_blank">
+                    <FontAwesomeIcon className="svg" icon={faLinkedinIn} />
+                </Link>
             </div>
         </div>
     </nav>)

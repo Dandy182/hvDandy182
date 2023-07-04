dc5c5f949739f82b5a71aba4bfdf3c03718573cf
daniel yesid cely robayo <danielcely1@gmail.com>
2023-07-04 00:38:22 -0500

    agregado pagina hv Daniel

--- src/MisDatos.js
+++ src/MisDatos.js
@@ -3,6 +3,7 @@ import freelancer from '../src/img/freelancer.png';
 import BlogDeCafe from '../src/img/BlogDeCafé.png';
 import FrontEndStore from '../src/img/FrontEndStore.png';
 import mBH from '../src/img/mom,baby &home.png';
+import dandy182 from '../src/img/Dandy182.png';
 
 const yo = {
     nombre:"Daniel Yesid Cely Robayo",
@@ -13,8 +14,8 @@ const yo = {
     github:"https://github.com/Dandy182",
     tecnologias: [{nombre:'html', img:'https://cdn-icons-png.flaticon.com/512/1216/1216733.png?w=826&t=st=1687437415~exp=1687438015~hmac=cad9fe2533ba0124702ea01fa5515e0b2976d9b76a527d0fe322402ccb4a2979'}, {nombre:'css', img:'https://cdn-icons-png.flaticon.com/512/732/732190.png'}, {nombre:'sass', img:'https://sass-lang.com/assets/img/logos/logo.svg'}, {nombre:'javascript', img:'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg'}, {nombre:'react', img:'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'}, {nombre:'redux', img:'https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png?20180308172936'}, {nombre:'nodeJS', img:'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'}, {nombre:'express', img:'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png'},{nombre:'posgres', img:'https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg'}, {nombre:'git', img:'https://git-scm.com/images/logo@2x.png'}],
     about:['Soy tecnologo en mantenimiento de equipos de computo y diseño e instalación de cableado estructurado con mas de 10 años de experiencia en soporte técnico, mesa de ayuda y montaje de sistemas de videoconferencias y transmisión de audio y video. ', 'Siempre me ha llamado la atención aprender a programar, pero por motivos económicos no pude acabar la ingeniería, Durante la época de pandemia pude dedicarme a aprender las bases para ser un desarrollador y algunas tecnologias base para el desarrollo web como son HTML y CSS y en 2022 gracias a un buen amigo y compañero de trabajo empezamos a realizar el bootcamp Henry', 'En mi paso por el bootcamp aprendí  muchas cosas como trabajar en equipo, nuevas herramientas como React, Redux, Node, Postgres, sequelize, asi como metodologías como scrum que me permiten realizar proyectos mas profesionales y de mejor calidad y sigo aprendiendo y adquiriendo experiencia y nuevos conocimientos para desempeñarme de manera optima en el mercado laboral '],
-    portafolio:[
-    {nombre:'Mom, Baby & Home', link:`https://mombabyandhome.netlify.app`, github:'https://github.com/Dandy182/vite-front-PF', tecnologias:'React, Redux Toolkit, Firebase, Stripe, Sweetaler2, Slick Carousel, Material UI y CSS modules', img:mBH
+    portafolio:[{nombre:'HvDaniel', link:`https://dandy182.netlify.app`, github:'https://github.com/Dandy182/hvDandy182', tecnologias:'React & Sass', img:dandy182
+    }, {nombre:'Mom, Baby & Home', link:`https://mombabyandhome.netlify.app`, github:'https://github.com/Dandy182/vite-front-PF', tecnologias:'React, Redux Toolkit, Firebase, Stripe, Sweetaler2, Slick Carousel, Material UI y CSS modules', img:mBH
 }, {nombre:'freelancer', link:`https://dandy182.github.io/Freelancer/`, github:'https://github.com/Dandy182/Freelancer', tecnologias:'HTML & CSS', img:freelancer
 }, {nombre:'blogDeCefé', link:`https://dandy182.github.io/blog_cafe/`, github:'https://github.com/Dandy182/blog_cafe', tecnologias:'HTML & CSS', img:BlogDeCafe
 }, {nombre:'FrontEnd Store', link:`https://dandy182.github.io/FrontEnd_Store/`, github:'https://github.com/Dandy182/FrontEnd_Store', tecnologias:'HTML & CSS', img:FrontEndStore

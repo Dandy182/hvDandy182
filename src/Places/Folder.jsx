ae1d0de2284895b6453cbf5da1be3efff4da9295
daniel yesid cely robayo <danielcely1@gmail.com>
2023-07-03 21:40:54 -0500

    portafolio listo

--- src/Places/Folder.jsx
+++ src/Places/Folder.jsx
@@ -2,21 +2,36 @@ import React from "react";
 import UpperBar from "../Components/UpperBar";
 import {motion} from 'framer-motion'
 import yo from '../MisDatos';
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+
+
+
 
 export default function Folder(){
 
     return(<motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
         <UpperBar />
-        <main className="contenedor spaced">
+        <main className="contenedor projects">
             <h2>Mi Portafolio</h2>
             <div className="projectosGeneral">
                 {
                     yo.portafolio.map((p, i) => {
-                        return (<a href={p.link} target="_blank">
-                        <div className="projectData" key={i}>
-                        <img src={p.img} alt={p.nombre} />
+                        return (<div><a href={p.link} className="linkProject" key={i} >
+                            <div className="projectData" key={i}>
+                                <img src={p.img} alt={p.nombre} />
                        </div>
-                        </a>)
+                        </a>
+                         <div className="cover" key={i}>
+                            <p><span>Proyecto: </span> {p.nombre}</p>
+                            <p><span>Tecnologias: </span> {p.tecnologias}</p>
+                            <Link target="_blank" className="icon" to={p.github}>
+                                <FontAwesomeIcon className="svg" icon={faGithub} />
+                                <p>Repo</p>
+                            </Link>
+                            </div>
+                        </div>)
                     })
   
                 }
